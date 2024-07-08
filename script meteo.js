cda6c44b0ddd7f2b014ba21c69a58845c9faa157
NatDI <winter2334@gmx.fr>
2024-07-08 16:42:33 +0200

    Add files via upload

---
+++ script meteo.js
@@ -0,0 +1,72 @@
+const api = {
+    endpoint: "https://api.openweathermap.org/data/2.5/",
+    key: "5a4d16aa8f41889e5d08d33b4cb0b4bf"
+  }
+   // console.log(api)
+
+
+const input = document.querySelector("#input");
+const temperature = document.querySelector("#temperattrue");
+input.addEventListener("keypress", enter);
+
+function enter(e) {
+    if (e.keyCode === 13) {
+      getInfo(input.value);
+    }
+  }
+
+async function getInfo (data) {
+  const res = await fetch(`${api.endpoint}weather?q=${data}&units=metric&appID=${api.key}`);
+  const result = await res.json();
+
+displayResult(result);
+}
+
+function displayResult (result){
+  console.log(result);
+  let city = document.querySelector("#city");
+  city.textContent = `${result.name}, ${result.sys.country}`;
+
+  getOurDate();
+
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = `${Math.round(result.main.temp)} <span>°</span>`;
+
+  let feelsLike = document.querySelector("#feelsLike");
+  feelsLike.innerHTML = ` <span> Feels like: </span>${Math.round(result.main.feels_like)}<span>°</span>`;
+
+  let conditions = document.querySelector ("#conditions");
+  //let conditions1 = conditions.toUpperCase();
+  conditions.textContent = `${result.weather[0].description}`;
+
+   
+  let variation = document.querySelector("#variation");
+  variation.innerHTML = "Min: " +`${Math.round(result.main.temp_min)} <span>°</span>` + " " + "Max: " + `${Math.round(result.main.temp_max)}<span>°</span>`;
+}
+
+
+function getOurDate(){
+  
+const myDate = new Date();
+
+const days = ["Sunday", "Lundi", "Mardi", "Mercredi", "Jeudi", "Friday", "Samedi"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//let date = document.querySelector("#date").textContent = days[myDate.getDay()];
+let day = days[myDate.getDay()];
+console.log (day);
+
+let todayDate = myDate.getDate();
+console.log(todayDate);
+
+
+let month = months[myDate.getMonth()];
+console.log(month);
+
+let year = myDate.getFullYear();
+console.log(year);
+
+let showDate = document.querySelector("#date");
+showDate.textContent = `${day}` + " " + `${todayDate}` + " " + `${month}` + " " + `${year}`
+
+}
+
