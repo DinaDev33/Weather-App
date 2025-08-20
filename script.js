e012040ac7f3b5c62c06f6c47ebd5e0250c61811
DinaDev33 <winter2334@gmx.fr>
2025-08-20 22:59:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -64,8 +64,10 @@ function getOurDate(){
   
 const myDate = new Date();
 
-const days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Friday", "Samedi"];
-const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", 
+                "July", "August", "September", "October", "November", "December"];
+ 
 //let date = document.querySelector("#date").textContent = days[myDate.getDay()];
 let day = days[myDate.getDay()];
 console.log (day);
@@ -84,4 +86,5 @@ let showDate = document.querySelector("#date");
 showDate.textContent = `${day}` + " " + `${todayDate}` + " " + `${month}` + " " + `${year}`
 
 }
+
 
